156ac1cf0ba29210be2c4ad4fa81bb6d39eaadd3
bhangdante@gmail.com <87205105+dantebhang@users.noreply.github.com>
2022-03-08 18:28:58 -0800

    start of exchange page

--- src/components/Exchanges.js
+++ src/components/Exchanges.js
@@ -1,23 +1,48 @@
-import React from 'react';
-import millify from 'millify';
-import { Collapse, Row, Col, Typography, Avatar } from 'antd';
-import HTMLReactParser from 'html-react-parser';
+import React from "react";
+import millify from "millify";
+import { Collapse, Row, Col, Typography, Avatar } from "antd";
+import HTMLReactParser from "html-react-parser";
 
-import { useGetExchangesQuery } from '../services/exchangesApi';
-import Loader from './Loader';
+import { useGetExchangesQuery } from "../services/exchangesApi";
+import Loader from "./Loader";
 
 const { Text } = Typography;
 const { Panel } = Collapse;
 
 function Exchanges() {
-  const {exchangesList, isFetching } = useGetExchangesQuery();
-  
+	const { data, isFetching } = useGetExchangesQuery();
+  const exchangesList = data;
 
-  if (isFetching) return <Loader />;
+	if (isFetching) return <Loader />;
 
-  return (
-    <div>Exchanges</div>
-  )
+	return (
+		<>
+			<Row>
+				<Col span={6}>Exchanges</Col>
+				<Col span={6}>24h Trade Volume</Col>
+				<Col span={6}>Markets</Col>
+				<Col span={6}>Change</Col>
+			</Row>
+      <Row>
+        {exchangesList.map((exchange) => (
+          <Col span={24}>
+          <Collapse>
+          <Panel key={exchange.id} showArrow={false} header={(
+            <Row key={exchange.id}>
+              <Col span={6}>
+                <Text>{exchange.trust_score_rank}</Text>
+              </Col>
+
+            </Row>
+          )}>
+
+          </Panel>
+          </Collapse>
+          </Col>
+        ))}
+      </Row>
+		</>
+	);
 }
 
-export default Exchanges
+export default Exchanges;
